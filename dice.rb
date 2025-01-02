ecb615850be1141ce73ebd10b7b4c468a4c0c09f
Sir Bedolla <69206592+erick38@users.noreply.github.com>
2025-01-02 05:51:22 +0000

    completed dice links

--- dice.rb
+++ dice.rb
@@ -1,4 +1,66 @@
 require "sinatra"
-get '/' do
-  "Hello World"
+require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+get("/") do
+  "
+  <h1>Dice Roll</h1>
+  <ul>
+    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
+    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
+  </ul>
+  "
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d10</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/5/4") do
+  first_die = rand(1..4)
+  first_2 = rand(1..4)
+  first_3 = rand(1..4)
+  first_4 = rand(1..4)
+  first_5 = rand(1..4)
+
+  sum = first_die + first_2 + first_3 + first_4 + first_5
+	
+  outcome = "You rolled a #{first_die}, #{first_2}, #{first_3}, #{first_4}, and a #{first_5} for a total of #{sum}."
+	
+  "<h1>5d4</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/1/20") do
+  first_die = rand(1..20)
+  sum = first_die
+	
+  outcome = "You rolled a #{first_die} for a total of #{sum}."
+	
+  "<h1>1d20</h1>
+   <p>#{outcome}</p>"
 end
